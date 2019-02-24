0b472081b6a0b85b14e30dc6ef2526a2c3443244
Nick Hanselman <nickhanselman@icloud.com>
2019-02-24 03:06:24 -0700

    implemented a bunch of stuff

--- scripts/players.js
+++ scripts/players.js
@@ -1,4 +1,5 @@
 var players_json = require("../resources/json/players.json");
+var fighters_json = require("../resources/json/fighters.json");
 var generic_json = require("../resources/json/generic.json");
 
 var fs = require("fs");
@@ -6,10 +7,17 @@ var fs = require("fs");
 function populateDropdown() {
   var dropdownItems = "";
   for (var player in players_json) {
-    dropdownItems += "<a class=\"dropdown-item\" href=\"#\" onclick='displayPlayerStats(" + player + ")'>" + player + "</a><div class=\"dropdown-divider\"></div>";
+    dropdownItems += "<a class=\"dropdown-item\" href=\"#\" onclick='displayPlayerStats(\"" + player + "\")'>" + player + "</a><div class=\"dropdown-divider\"></div>";
   }
   console.log(dropdownItems);
   document.getElementById("player-dropdown").innerHTML = dropdownItems;
+}
+
+function savePlayers(json) {
+  fs.writeFile(__dirname + '/../resources/json/players.json', JSON.stringify(json), function (e) {
+    if (e) throw e;
+    console.log('Player Saved!');
+  });
 }
 
 function hasWhiteSpace(s) {
@@ -37,11 +45,62 @@ function createNewPlayer() {
   players_json[playerID] = generic_json;
   // console.log(players_json[playerID]);
   // console.log(Object.keys(players_json));
-
-  fs.writeFile(__dirname + '/../resources/json/players.json', JSON.stringify(players_json), function (e) {
-    if (e) throw e;
-    console.log('Player Saved!');
-    alert("Player Created!");
-  });
+  savePlayers(players_json);
+  alert("Player Created!");
   document.getElementById("newPlayer").value = "";
+}
+
+function getMostPlayed(playerKey, json) {
+  var mostPlayed = json[playerKey].most_used;
+  for (var key in json[playerKey].fighters) {
+    // console.log(key + ": " + json[playerKey].fighters[key].times_used + " | " + mostPlayed + ": " + json[playerKey].fighters[mostPlayed].times_used);
+    if (json[playerKey].fighters[key].times_used > json[playerKey].fighters[mostPlayed].times_used) {
+      mostPlayed = key;
+    }
+  }
+  console.log("Most Played: " + mostPlayed);
+  json[playerKey].most_used = mostPlayed;
+  savePlayers(json);
+}
+
+function getWorstPlayed(playerKey, json) {
+  var worstPlayed = json[playerKey].worst;
+  for (var key in json[playerKey].fighters) {
+    // console.log(key + ": " + json[playerKey].fighters[key].losses + " | " + worstPlayed + ": " + json[playerKey].fighters[worstPlayed].losses);
+    if (json[playerKey].fighters[key].losses > json[playerKey].fighters[worstPlayed].losses) {
+      worstPlayed = key;
+    }
+  }
+  console.log("Worst Played: " + worstPlayed);
+  json[playerKey].worst = worstPlayed;
+  savePlayers(json);
+}
+
+function getBestPlayed(playerKey, json) {
+  var bestPlayed = json[playerKey].best;
+  for (var key in json[playerKey].fighters) {
+    // console.log(key + ": " + json[playerKey].fighters[key].wins + " | " + bestPlayed + ": " + json[playerKey].fighters[bestPlayed].wins);
+    if (json[playerKey].fighters[key].wins > json[playerKey].fighters[bestPlayed].wins) {
+      bestPlayed = key;
+    }
+  }
+  console.log("Best Played: " + bestPlayed);
+  json[playerKey].best = bestPlayed;
+  savePlayers(json);
+}
+
+function displayPlayerStats(key) {
+  console.log(key);
+  getMostPlayed(key, players_json);
+  getBestPlayed(key, players_json);
+  getWorstPlayed(key, players_json);
+  document.getElementById("p-name").innerHTML = key;
+  document.getElementById("p-total").innerHTML = players_json[key].total_plays;
+  document.getElementById("p-wins").innerHTML = players_json[key].wins;
+  document.getElementById("p-losses").innerHTML = players_json[key].losses;
+  document.getElementById("p-most").innerHTML = players_json[key].most_used;
+  document.getElementById("p-best").innerHTML = players_json[key].best;
+  document.getElementById("p-worst").innerHTML = players_json[key].worst;
+  document.getElementById("fighter-image").src = "../" + fighters_json[players_json[key].most_used].image;
+  document.getElementById("fighter-image").alt = fighters_json[players_json[key].most_used].name;
 }
